8bf072e0f74caa99f838468c3d662cab2641b69f
khanhthanhdev <thanhkt27507@gmail.com>
2024-11-15 15:35:24 +0700

    add node interface, double click to zoom in center

--- app/workflow/_components/FlowEditor.tsx
+++ app/workflow/_components/FlowEditor.tsx
@@ -5,18 +5,20 @@ import { Background, BackgroundVariant, Controls, ReactFlow, useEdgesState, useN
 import React from 'react'
 
 import "@xyflow/react/dist/style.css"
+import { CreateFlowNode } from '@/lib/workflow/createFlowNode'
+import { TaskType } from '@/types/task'
+import NodeComponent from './nodes/NodeComponent'
+
+const nodeTypes = {
+    NexCrawlNode: NodeComponent,
+}
 
 function FlowEditor({workflow}: {workflow: Workflow}) {
 
     const [nodes, setNodes, onNotesChange] = useNodesState([
-        {
-            id: '1',
-            position: { x: 0, y: 0 },
-            data: { 
-                label: 'example' 
-            },
-        },
+        CreateFlowNode(TaskType.LAUNCH_BROWSER)
     ])
+        
 
     const [edges, setEdges, onEdgesChange] = useEdgesState([])
 
@@ -26,6 +28,7 @@ function FlowEditor({workflow}: {workflow: Workflow}) {
             edges={edges}
             onNodesChange={onNotesChange}
             onEdgesChange={onEdgesChange}
+            nodeTypes={nodeTypes}
         >
             <Controls position='top-left' />
             <Background variant={BackgroundVariant.Dots} gap={12} size={1} />
