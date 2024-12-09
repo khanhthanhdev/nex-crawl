41e84015186313867923859493a03acd20f78322
khanhthanhdev <thanhkt27507@gmail.com>
2024-12-09 13:48:12 +0700

    preview phase details

--- app/workflow/runs/[workflowId]/[executionId]/ExecutionViewer.tsx
+++ app/workflow/runs/[workflowId]/[executionId]/ExecutionViewer.tsx
@@ -3,6 +3,7 @@
 "use client"
 
 import { GetWorkflowExecutionWithPhases } from "@/actions/workflows/getWorkflowExecutionWithPhases"
+import { GetWorkflowPhaseDetails } from "@/actions/workflows/getWorkflowPhaseDetails"
 import { Badge } from "@/components/ui/badge"
 import { Button } from "@/components/ui/button"
 import { Separator } from "@/components/ui/separator"
@@ -12,7 +13,7 @@ import { WorkflowExecutionStatus } from "@/types/workflow"
 import { useQuery } from "@tanstack/react-query"
 import { formatDistanceToNow } from "date-fns"
 import {CalendarIcon, CircleDashedIcon, ClockIcon, CoinsIcon, Loader2Icon, LucideIcon, WorkflowIcon } from "lucide-react"
-import { ReactNode } from "react"
+import { ReactNode, useState } from "react"
 
 type ExecutionData = Awaited<ReturnType<typeof GetWorkflowExecutionWithPhases>>
 
@@ -20,11 +21,22 @@ type ExecutionData = Awaited<ReturnType<typeof GetWorkflowExecutionWithPhases>>
 export default function ExecutionViewer(
     { initialData }: { initialData: ExecutionData }
 ) {
+
+    const [selectedPhase, setSelectedPhase] = useState<string | null>(null)
+
     const query = useQuery({
         queryKey: ["execution", initialData?.id],
         initialData,
         queryFn: () => GetWorkflowExecutionWithPhases(initialData!.id),
         refetchInterval: (q) => q.state.data?.status === WorkflowExecutionStatus.RUNNING ? 1000 : false
+    })
+
+    const isRunning = query.data?.status === WorkflowExecutionStatus.RUNNING;
+
+    const phaseDetails  = useQuery({
+        queryKey: ["phaseDetails", selectedPhase],
+        enabled: selectedPhase !== null,
+        queryFn: () => GetWorkflowPhaseDetails(selectedPhase!)
     })
 
     const duration = DatesToDurationString(
@@ -65,7 +77,14 @@ export default function ExecutionViewer(
             <Separator />
             <div className="overflow-auto h-full px-2 py-4">
                 {query.data?.phases.map((phase, index) => (
-                    <Button key={phase.id} className="w-full justify-between" variant={"ghost"}>
+                    <Button key={phase.id} className="w-full justify-between" variant={selectedPhase === phase.id ? "secondary" : "ghost"}
+                    onClick={() => {
+                        if (isRunning) return;
+                        setSelectedPhase(phase.id)
+                        
+                        
+                    }}
+                    >
                         <div className="flex items-center gap-2">
                             <Badge variant={"outline"} >
                                 {index + 1}
@@ -81,6 +100,9 @@ export default function ExecutionViewer(
                 ))}
             </div>
         </aside>
+        <div className="flex w-full h-full">
+                <pre>{JSON.stringify(phaseDetails.data, null, 4)}</pre>
+        </div>
     </div>
 }
 
