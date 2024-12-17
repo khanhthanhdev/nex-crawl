b6c4c520161bf3586cc167f9e9a14c84cdd6a20f
khanhthanhdev <thanhkt27507@gmail.com>
2024-12-17 14:45:16 +0700

    comple Runs table, onlick to navigate to selected run

--- app/workflow/runs/[workflowId]/_components/ExecutionsTable.tsx
+++ app/workflow/runs/[workflowId]/_components/ExecutionsTable.tsx
@@ -6,6 +6,11 @@ import { Table, TableBody, TableHead, TableHeader, TableRow, TableCell } from '@
 import { DatesToDurationString } from '@/lib/helper/dates';
 import { useQuery } from '@tanstack/react-query';
 import React from 'react'
+import ExecutionStatusIndicator from './ExecutionStatusIndicator';
+import { WorkflowExecutionStatus } from '@/types/workflow';
+import { CoinsIcon } from 'lucide-react';
+import { formatDistanceToNow } from 'date-fns';
+import { useRouter } from 'next/navigation';
 
 type InitialDataType = Awaited<ReturnType<typeof GetWorkflowExecutions>>
 
@@ -16,6 +21,8 @@ function ExecutionsTable({
     workflowId: string;
     initialData: InitialDataType
 }) {
+
+    const router = useRouter();
 
     const query = useQuery({
         queryKey: ["executions", workflowId],
@@ -37,8 +44,14 @@ function ExecutionsTable({
         <TableBody className='gap-2 h-full overflow-auto'>
             {query.data.map((execution) => {
                 const duration = DatesToDurationString(execution.completedAt, execution.startedAt)
+                
+                const formattedStartedAt = execution.startedAt  && formatDistanceToNow(execution.startedAt, { addSuffix: true })
 
-                return <TableRow key={execution.id}>
+                return <TableRow key={execution.id} className='cursor-pointer'
+                onClick={() => {
+                    router.push(`/workflow/runs/${execution.workflowId}/${execution.id}`)
+                }}
+                >
                     <TableCell>
                         <div className='flex flex-col'>
                             <span className='font-semibold'>
@@ -51,12 +64,32 @@ function ExecutionsTable({
                         </div>
                     </TableCell>
                     <TableCell>
-                        <div>
-                            <div>{execution.status}</div>
-                            <div>{duration}</div>
+                        <div className='flex flex-col'>
+                            <div className='flex gap-2 items-center'>
+                                <ExecutionStatusIndicator status={execution.status as WorkflowExecutionStatus} />
+                                <span className='font-semibold capitalize' >
+                                    {execution.status}
+                                </span>
+                            </div>
+                            <div className='text-muted-foreground text-xs mx-5'>{duration}</div>
                         </div>
+                        
                     </TableCell>
-                    
+                    <TableCell>
+                        <div className='flex flex-col'>
+                            <div className='flex gap-2 items-center'>
+                                <CoinsIcon size={16} className='text-primary' />
+                                <span className='font-semibold capitalize' >
+                                    {execution.creditsConsumed}
+                                </span>
+                            </div>
+                            <div className='text-muted-foreground text-xs mx-5'>Credits</div>
+                        </div>
+                        
+                    </TableCell>
+                    <TableCell className='text-right text-muted-foreground'>
+                        {formattedStartedAt}
+                    </TableCell>
                 </TableRow>
             })}
         </TableBody>
