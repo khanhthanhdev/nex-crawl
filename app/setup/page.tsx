26ef60a2450623372f86a29029e9f2ec101d5ece
khanhthanhdev <thanhkt27507@gmail.com>
2025-01-03 15:35:22 +0700

    add setupPage, auto add 100 credit when sign up

---
+++ app/setup/page.tsx
@@ -0,0 +1,7 @@
+import { SetupUser } from "@/actions/billing/setupUser";
+import { waitFor } from "@/lib/helper/waitFor";
+
+export default async function SetupPage(){
+    return await SetupUser();
+}
+
